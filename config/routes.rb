f84bfda26884cb352eb0dd33b70379a28e55dae6
masaaki1915 <masaaki1915@gmail.com>
2017-05-09 17:14:09 +0900

    users ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  resources :users do
+  resources :users, only: %i(index edit) do
     resources :actions, only: %i(index)
     resources :lists do
       resources :items, only: %i(index)
     end
   end
   resources :items, except: %i(index)
+
+  get 'users/:item_id/done' => 'users#done'
+  get 'users/:item_id/doing' => 'users#doing'
+  get 'users/:user_id/following' => 'users#following'
+  get 'users/:user_id/follower' => 'users#follower'
 end
